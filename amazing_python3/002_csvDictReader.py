7359377896ef86ab9fc7da8a82d016a2287c1851
luke-cao <liangkuncao@gmail.com>
2019-08-27 08:22:01 +0800

    amazing_python 3

---
+++ amazing_python3/002_csvDictReader.py
@@ -0,0 +1,9 @@
+import csv
+
+
+with open('002_csvfile.csv') as data:
+    reader = csv.DictReader(data)
+    for row in reader:
+        date = row['date']
+        convert = float(row['clicks']) / float(row['display'])
+        print(f'On {date}, the convert rate is {convert * 100: .2f}%')
