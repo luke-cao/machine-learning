148cea83527065968682b93e7404a61e35a7047e
luke-cao <liangkuncao@gmail.com>
2019-09-01 22:00:51 +0800

    hell_java

--- hello_java/httpclient/src/main/java/RequestExecution.java
+++ hello_java/httpclient/src/main/java/RequestExecution.java
@@ -1,23 +1,9 @@
-package lukecao;
-
-import org.apache.http.HttpEntity;
-import org.apache.http.NameValuePair;
-import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.fluent.Content;
 import org.apache.http.client.fluent.Form;
 import org.apache.http.client.fluent.Request;
-import org.apache.http.client.methods.CloseableHttpResponse;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClients;
-import org.apache.http.message.BasicNameValuePair;
-import org.apache.http.util.EntityUtils;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.ArrayList;
-import java.util.List;
 
 public class RequestExecution {
     public static void main(String[] args) throws IOException {
