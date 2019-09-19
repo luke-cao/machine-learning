4992461d30bf1f2519044f59756821c1262e0f3e
luke-cao <liangkuncao@gmail.com>
2019-09-20 00:09:32 +0800

    bubble sort 2 impls

---
+++ hello_python/011_bubble_sort.py
@@ -0,0 +1,38 @@
+import datetime
+
+
+def bubble_sort(arr):
+    start = datetime.datetime.today()
+    for i in range(len(arr)):
+        for j in range(len(arr) - i - 1):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+    end = datetime.datetime.today()
+    print(f'bubble_sort: {end - start}')
+    return arr
+
+
+def bubble_sort2(arr):
+    start = datetime.datetime.today()
+    n = len(arr)
+
+    def swap(i, j):
+        arr[i], arr[j] = arr[j], arr[i]
+
+    x = -1
+    swapped = True
+    while swapped:
+        swapped = False
+        x += 1
+        for i in range(1, n - x):
+            if arr[i - 1] > arr[i]:
+                swap(i - 1, i)
+                swapped = True
+    end = datetime.datetime.today()
+    print(f'bubble_sort2: {end - start}')
+    return arr
+
+
+array = [1, 3, 5, 7, 8, 3, 4] * 100
+print(bubble_sort(array))
+print(bubble_sort2(array))
