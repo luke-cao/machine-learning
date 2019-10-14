46f9436bcbbdeb1dae84d3a213bda459b37b66d1
luke-cao <liangkuncao@gmail.com>
2019-10-15 00:18:16 +0800

    count primes

--- leetcode/0204_count_primes.py
+++ leetcode/0204_count_primes.py
@@ -1,2 +1,18 @@
+import math
+
 def countPrimes(n: int) -> int:
-    pass
+    primes = [True for _ in range(n)]
+
+    for i in range(2, n):
+        if i ** 2 >= n:
+            break
+        if primes[i]:
+            for j in range(i, n):
+                if i * j >= n:
+                    break
+                primes[i * j] = False
+    # print(primes)
+    return sum(primes[2:])
+
+
+print(countPrimes(2))
