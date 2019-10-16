40720bc45d25cf9174f59ab76590f64992a16f91
luke-cao <liangkuncao@gmail.com>
2019-10-16 22:57:35 +0800

    mojority element

--- leetcode/0169_majority_element_easy.py
+++ leetcode/0169_majority_element_easy.py
@@ -6,19 +6,31 @@ from collections import Counter
 #         counter = Counter(nums)
 #         return counter.most_common(1)[0][0]
 
+# class Solution:
+#     def majorityElement(self, nums: List[int]) -> int:
+#         halfline = len(nums) / 2
+#         counter = {}
+#         for i in nums:
+#             if i in counter:
+#                 counter[i] = counter[i] + 1
+#             else:
+#                 counter[i] = 1
+#             if counter.get(i) > halfline:
+#                 return i
+#         return 'No Majority'
+
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        halfline = len(nums) // 2
-        counter = {}
-        for i in nums:
-            if i in counter:
-                counter[i] = counter[i] + 1
+        index, cnt = 0, 1
+        for i in range(1, len(nums)):
+            if nums[i] == nums[index]:
+                cnt += 1
             else:
-                counter[i] = 1
-            if counter.get(i) > halfline:
-                return i
-        return 'No Majority'
+                cnt -= 1
+                if cnt == 0:
+                    index, cnt = i, 1
+        return nums[index]
 
 
 
-print(Solution().majorityElement([1]))
+print(Solution().majorityElement([2,2,1,1,1,1, 2,2]))
