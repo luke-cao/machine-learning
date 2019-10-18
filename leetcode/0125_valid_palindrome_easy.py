7bad438e0b8090ce057d623a73f383e3008135a9
luke-cao <liangkuncao@gmail.com>
2019-10-19 01:29:21 +0800

    valid palindrome

---
+++ leetcode/0125_valid_palindrome_easy.py
@@ -0,0 +1,23 @@
+import re
+
+# class Solution:
+#     def isPalindrome(self, s: str) -> bool:
+#         s = re.sub(r'[^a-zA-Z0-9]+', '', s)
+#         s = s.lower()
+#         return s[::1] == s[::-1]
+
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        i, j = 0, len(s) - 1
+        while i < j:
+            while i < j and not s[i].isalnum():
+                i += 1
+            while i < j and not s[j].isalnum():
+                j -= 1
+            if s[i].lower() != s[j].lower():
+                return False
+            i += 1
+            j -= 1
+        return True
+
+print(Solution().isPalindrome("A man, a plan, a canal: Panama"))
