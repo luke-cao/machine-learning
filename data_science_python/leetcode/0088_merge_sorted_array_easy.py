2883b5db31260d966bab3089022bab01b15ca22f
luke-cao <liangkuncao@gmail.com>
2019-10-21 21:40:59 +0800

    merge two sorted arrays

--- data_science_python/leetcode/0088_merge_sorted_array_easy.py
+++ data_science_python/leetcode/0088_merge_sorted_array_easy.py
@@ -6,18 +6,15 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-        idx_nums1 = 0
-        idx_nums2 = 0
-        for idx_nums1 in range(m):
-            if idx_nums2 == n:
-                break
-            if nums2[idx_nums2] >= nums1[m - 1 + idx_nums2]:
-                break
-            if nums2[idx_nums2] < nums1[idx_nums1 + idx_nums2]:
-                nums1.pop()
-                nums1.insert(idx_nums1 + idx_nums2, nums2[idx_nums2])
-                idx_nums2 += 1
-        nums1[idx_nums1 + idx_nums2 + 1:] = nums2[idx_nums2:]
+        while m > 0 and n > 0:
+            if nums1[m - 1] < nums2[n - 1]:
+                nums1[m - 1 + n] = nums2[n - 1]
+                n -= 1
+            else:
+                nums1[m - 1 + n], nums1[m - 1] = nums1[m - 1], nums1[m - 1 + n]
+                m -= 1
+        if m == 0 and n > 0:
+            nums1[:n] = nums2[:n]
 
 
 if __name__ == '__main__':
