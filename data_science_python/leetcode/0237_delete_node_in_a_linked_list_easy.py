4076dcd049adaa54f836441f4229b367cd0ef74e
luke-cao <liangkuncao@gmail.com>
2019-10-25 01:19:52 +0800

    delete node

---
+++ data_science_python/leetcode/0237_delete_node_in_a_linked_list_easy.py
@@ -0,0 +1,33 @@
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution:
+    def deleteNode(self, node):
+        """
+        :type node: ListNode
+        :rtype: void Do not return anything, modify node in-place instead.
+        """
+        if node.next:
+            node.val = node.next.val
+        if node.next.next is None:
+            node.next = None
+            return
+        return self.deleteNode(node.next)
+
+
+class Solution:
+    def deleteNode(self, node):
+        """
+        :type node: ListNode
+        :rtype: void Do not return anything, modify node in-place instead.
+        """
+        while node.next:
+            node.val = node.next.val
+            if node.next.next is None:
+                node.next = None
+            if node.next:
+                node = node.next
+
