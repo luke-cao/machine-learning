6f746d8119e84e8a87552d4b5be4d483b3ebe880
luke-cao <liangkuncao@gmail.com>
2019-11-24 01:18:47 +0800

    heap

---
+++ data_science_python/leetcode/0215_kth_largest_element_in_an_array_medium.py
@@ -0,0 +1,11 @@
+from typing import List
+import heapq
+
+
+class Solution:
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        heapq.heapify(nums)
+        return heapq.nlargest(k,nums)[-1]
+
+
+print(Solution().findKthLargest([2, 1], 2))
